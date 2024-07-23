9c66878c4ee42e32eef48b0d7cd729dd9754ec7b
pepeboss <pepeboss114@gmail.com>
2024-07-23 23:14:19 +0200

    se añadio los CORS_ALLOW_METHODS, tambien se agrego los CORS_ALLOW_HEADERS, y se cambio el orden del common middleware en MIDDLEWARE = []

--- proyecto_Django/backSign/backSign/settings.py
+++ proyecto_Django/backSign/backSign/settings.py
@@ -45,12 +45,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'backSign.urls'
@@ -94,6 +94,24 @@ DATABASES = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
+
+CORS_ALLOW_METHODS = (
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+)
+
+CORS_ALLOW_HEADERS = (
+    "accept",
+    "authorization",
+    "content-type",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+)
 
 CORS_ALLOW_ALL_ORIGINS = True
 
